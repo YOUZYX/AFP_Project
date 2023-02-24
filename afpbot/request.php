6b85a88516db13642cf417b2bc7d431e6eca20f3
YOUNESS ZYADI <foryounes18@gmail.com>
2023-02-24 19:25:07 +0100

    add afp project

---
+++ afpbot/request.php
@@ -0,0 +1,26 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+use Orhanerday\OpenAi\OpenAi;
+
+$open_ai = new OpenAi('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');
+// get prompt parameter
+$prompt = $_GET['prompt'];
+// set api data
+$complete = $open_ai->completion([
+    'model' => 'text-davinci-003',
+    'prompt' => $prompt,
+    'temperature' => 0.7,
+    'max_tokens' => 256,
+    'top_p' => 1,
+    'frequency_penalty' => 0,
+    'presence_penalty' => 0,
+    'stream' => true
+], function ($curl_info, $data) {
+    // now we will get stream data
+    echo $data;
+    echo PHP_EOL;
+    ob_flush();
+    flush();
+    return strlen($data);
+});
